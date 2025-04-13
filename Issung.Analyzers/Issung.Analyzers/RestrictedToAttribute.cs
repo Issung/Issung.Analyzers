8861f83b0290e2475ee8870e123d430967a4af9c
Issung <joel@sonofgoran.com>
2025-04-13 18:08:32 +1000

    Initial commit

---
+++ Issung.Analyzers/Issung.Analyzers/RestrictedToAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Issung.Analyzers
+{
+    [RestrictedTo(typeof(Attribute))]   // The RestrictedToAttribute can only be applied to classes that extend Attribute.
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class RestrictedToAttribute(params Type[] allowedTypes) : Attribute
+    {
+        public Type[] AllowedTypes { get; } = allowedTypes;
+    }
+}
